9451edee3ab5dddc899375687a976f94be15dff6
guoshif <sanfeng@sf>
2024-01-04 12:24:00 +0800

    创建毕设项目

---
+++ SYZTB_IDEA_GSF/src/main/java/com/syztb_idea_gsf/BSApplication.java
@@ -0,0 +1,13 @@
+package com.syztb_idea_gsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSApplication.class, args);
+	}
+
+}
---
+++ SYZTB_IDEA_GSF/src/test/java/com/syztb_idea_gsf/BSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syztb_idea_gsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
